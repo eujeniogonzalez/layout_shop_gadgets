f179a881a5628ff12866eacd043b829ee9b8522a
Eujenio Gonzalez <vegas-host@yandex.ru>
2022-07-12 20:50:40 +0300

    Lesson 3 addition 1

--- src/index.js
+++ src/index.js
@@ -3,32 +3,35 @@ import './card.html'
 import './cart.html'
 import './index.scss'
 
-import Swiper, {Thumbs, Scrollbar} from 'swiper'
+import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/scrollbar'
+import 'swiper/css/pagination'
 
 const thumbSwiper = new Swiper('.card__slider-thumb', {
     spaceBetween: 44,
     slidesPerView: 3,
     scrollbar: {
         el: '.swiper-scrollbar',
-        draggable: true
+        draggable: true,
     },
-    modules: [Scrollbar]
+    modules: [Scrollbar],
 })
 new Swiper('.card__image', {
     spaceBetween: 10,
     slidesPerView: 1,
     thumbs: {
         swiper: thumbSwiper,
-        slideThumbActiveClass: 'card__thumb-btn_active'
+        slideThumbActiveClass: 'card__thumb-btn_active',
     },
-    modules: [Thumbs]
+    modules: [Thumbs],
 })
 new Swiper('.recomended__carousel', {
     spaceBetween: 30,
     slidesPerView: 5,
     pagination: {
         el: '.swiper-pagination',
-      },
+        type: 'bullets',
+    },
+    modules: [Pagination]
 })
